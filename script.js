cac66a0017042b6342d138b8c8fe3cd7d33069d7
mrinal-mann <mrinal.mann24@gmail.com>
2024-06-01 11:57:53 +0530

    final one

--- script.js
+++ script.js
@@ -1,40 +1,61 @@
-const input = document
-  .getElementById("audio")
-  .addEventListener("change", function (event) {
-    const file = event.target.files[0];
-    const reader = new FileReader();
+document.getElementById("audio").addEventListener("change", (event) => {
+    const file = event.target.files[0]
+  
+    const reader = new FileReader()
+  
     reader.addEventListener("load", (event) => {
-      const arraybuffer = event.target.result;
-      const audiocontext = new (window.AudioContext ||
-        window.webkitAudioContext)();
-      audiocontext.decodeAudioData(arraybuffer, (audiobuffer) => {
-        visualize(audiobuffer);
-      });
-    });
-    reader.readAsArrayBuffer(file);
-  });
-
-function visualize(audiobuffer) {
-  const canvas = document.getElementById("canvas");
-  canvas.width = 800;
-  canvas.height = 200;
-
-  const canvascontext = canvas.getContext("2d");
-  const channeldata = audiobuffer.getChannelData(0);
-
-  let numberofchunks = 400;
-  const chunksize = Math.ceil(channeldata.length / numberofchunks);
-  canvascontext.fillStyle = "#5271FF";
-
-  const center = canvas.height / 2;
-  const barwidth = canvas.width / numberofchunks;
-
-  for (let i = 0; i < numberofchunks; i++) {
-    const chunk = channeldata.slice(i * chunksize, (i + 1) * chunksize);
-
-    const min = Math.min(...chunk)*20;
-    const max = Math.max(...chunk)*20;
-
-    canvascontext.fillRect(i*barwidth,center-max,barwidth,max+Math.abs(min))
+      const arrayBuffer = event.target.result
+  
+      const audioContext = new (window.AudioContext ||
+        window.webkitAudioContext)()
+  
+      audioContext.decodeAudioData(arrayBuffer, (audioBuffer) => {
+        visualize(audioBuffer, audioContext)
+      })
+    })
+  
+    reader.readAsArrayBuffer(file)
+  })
+  
+  function visualize(audioBuffer, audioContext) {
+    const canvas = document.getElementById("canvas")
+    canvas.width = canvas.clientWidth
+    canvas.height = canvas.clientHeight
+  
+    const analyser = audioContext.createAnalyser()
+    analyser.fftSize = 256
+  
+    const frequencyBufferLength = analyser.frequencyBinCount
+    const frequencyData = new Uint8Array(frequencyBufferLength)
+  
+    const source = audioContext.createBufferSource()
+    source.buffer = audioBuffer
+    source.connect(analyser)
+    analyser.connect(audioContext.destination)
+    source.start()
+  
+    const canvasContext = canvas.getContext("2d")
+  
+    const barWidth = canvas.width / frequencyBufferLength
+  
+    function draw() {
+      requestAnimationFrame(draw)
+      canvasContext.fillStyle = "rgb(173, 216, 230)"
+      canvasContext.fillRect(0, 0, canvas.width, canvas.height)
+  
+      analyser.getByteFrequencyData(frequencyData)
+  
+      for (let i = 0; i < frequencyBufferLength; i++) {
+        // The frequency data is composed of integers on a scale from 0 to 255
+        canvasContext.fillStyle = "rgb(" + (frequencyData[i]) + ",118, 138)";
+        canvasContext.fillRect(
+          i * barWidth,
+          canvas.height - frequencyData[i],
+          barWidth - 1,
+          frequencyData[i]
+        )
+      }
+    }
+  
+    draw()
   }
-}
